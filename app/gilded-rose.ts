2fd5274dfdedbe8cb6b6f70f47505719d6984d17
Nerah <alascenci@gmail.com>
2020-09-20 21:49:52 +0200

    inversion pour if avec négation

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -46,19 +46,19 @@ export class GildedRose {
                 item.sellIn -= 1;
             }
             if (item.sellIn < 0) {
-                if (item.name != 'Aged Brie') {
-                    if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+                if (item.name == 'Aged Brie') {
+                    if (item.quality < 50) {
+                        item.quality += 1
+                    }
+                } else {
+                    if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+                        item.quality = 0
+                    } else {
                         if (item.quality > 0) {
                             if (item.name != 'Sulfuras, Hand of Ragnaros') {
                                 item.quality -= 1
                             }
                         }
-                    } else {
-                        item.quality = 0
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality += 1
                     }
                 }
             }
