ea14489be0667e620973ebe9c8ee67a9091f34c8
Nerah <alascenci@gmail.com>
2020-09-20 20:26:55 +0200

    Ajout test qualité sulfura

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,10 +3,10 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('Sulfura should keep its quality over time', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('foo');
+        expect(items[0].quality).to.equal(80);
     });
 
 });
