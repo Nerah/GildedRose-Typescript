8a253aca74e5732960abec563588631a1a9b5e40
Nerah <alascenci@gmail.com>
2020-09-20 22:34:54 +0200

    constructeur + methodes par defaut pour OrdinaryItem

--- app/OrdinaryItem.ts
+++ app/OrdinaryItem.ts
@@ -1,6 +1,18 @@
 import {CustomItem} from "./CustomItem";
+import {Item} from "./gilded-rose";
 
 export abstract class OrdinaryItem extends CustomItem {
+  protected readonly rules = {
+    quality: {
+      min: 0,
+      max: 50
+    }
+  };
+
+  protected constructor(item: Item) {
+    super(item);
+  }
+
   update(): void {
     this.updateSellIn();
     this.updateQuality();
@@ -8,4 +20,12 @@ export abstract class OrdinaryItem extends CustomItem {
 
   abstract updateSellIn(): void
   abstract updateQuality(): void
+
+  protected hasReachMinQuality(): boolean {
+    return this.item.quality < this.rules.quality.min;
+  }
+
+  protected hasReachMaxQuality(): boolean {
+    return this.item.quality > this.rules.quality.max;
+  }
 }
