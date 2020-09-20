796d53ee925a00ae263215f7c7ef5ca0fb43db9f
Nerah <alascenci@gmail.com>
2020-09-20 22:19:38 +0200

    Ajout constructeur LegendaryItem

--- app/LegendaryItem.ts
+++ app/LegendaryItem.ts
@@ -1,6 +1,11 @@
 import {CustomItem} from "./CustomItem";
+import {Item} from "./gilded-rose";
 
 export class LegendaryItem extends CustomItem {
+
+  constructor(item: Item) {
+    super(item);
+  }
   // every legendary item never age
   update(): void {
 
