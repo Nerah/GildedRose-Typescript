586a7e6c45fb7262f752878b9b8f15300a80dab3
Nerah <alascenci@gmail.com>
2020-09-20 20:47:20 +0200

    Ajouts test elixir of the mongoose.

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -23,4 +23,18 @@ describe('Gilded Rose', function () {
         expect(items[0].quality).to.equal(8);
     });
 
+    it('Elixir of the Mongoose quality should decrement by 1 if sell by date hasn\'t passed', function() {
+        const gildedRose = new GildedRose([ new Item("Elixir of the Mongoose", 10, 20) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(9);
+        expect(items[0].quality).to.equal(19);
+    });
+
+    it('Elixir of the Mongoose quality should decrement by 2 if sell by date has passed', function() {
+        const gildedRose = new GildedRose([ new Item("Elixir of the Mongoose", 0, 10) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(-1);
+        expect(items[0].quality).to.equal(8);
+    });
+
 });
